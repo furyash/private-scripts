63ebd1a618ddf68322ff28334deb4826d4b62c40
Yash Pokharel <36482644+furyash@users.noreply.github.com>
2021-11-13 11:34:25 +0545

    Update rename_from_list.py

--- rename_from_list.py
+++ rename_from_list.py
@@ -1,13 +1,13 @@
 import os
-filenames = next(os.walk("."), (None, None, []))[2]
+# import old file names
+filenames = os.listdir(".")
 filenames = [x for x in filenames if x[0] == 'l']
-
+#import new file names
 file = open('contents.txt', 'r+')
 new_filenames = []
 for line in file:
     new_filenames.append(line[:-1] + ".mp4")
-print(new_filenames)
-
+# rename it
 for i in range(79):
     if os.path.isfile(new_filenames[i]):
         print(new_filenames[i] + " already exists.")
